3d9a67414611bbd27c140aaf98d71d7114eb6619
asilmuhammadiy <asilbekyuldashev07@gmail.com>
2022-01-16 00:30:37 +0900

    separating directories with Path()

--- handlers/users/doc_handlers.py
+++ handlers/users/doc_handlers.py
@@ -2,6 +2,9 @@ from loader import dp, bot
 from aiogram.types import ContentType, Message
 from pathlib import Path
 
+download_path = Path().joinpath("downloads", "categories")
+download_path.mkdir(parents=True, exist_ok=True)
+
 
 @dp.message_handler()
 async def text_handler(message: Message):
@@ -10,7 +13,7 @@ async def text_handler(message: Message):
 
 @dp.message_handler(content_types=ContentType.DOCUMENT)
 async def doc_handler(message: Message):
-    await message.document.download()
+    await message.document.download(destination=download_path)
     doc_id = message.document.file_id
     await message.reply("Siz hujjat yubordingiz!\n"
                         f"file_id = {doc_id}")
@@ -19,11 +22,19 @@ async def doc_handler(message: Message):
 # @dp.message_handler(content_types=ContentType.VIDEO)
 @dp.message_handler(content_types='video')
 async def video_handler(message: Message):
-    await message.video.download()
+    await message.video.download(destination=download_path)
     await message.reply("Video qabul qilindi\n"
                     f"file_id = {message.video.file_id}")
 
 
+@dp.message_handler(content_types='photo')
+async def video_handler(message: Message):
+    await message.photo[-1].download(destination=download_path)
+    await message.reply("Rasm qabul qilindi\n"
+                    f"file_id = {message.photo[-1].file_id}")
 
 
+@dp.message_handler(content_types=ContentType.ANY)
+async def video_handler(message: Message):
+    await message.answer(f"{message.content_type} qabul qilindi")
 
